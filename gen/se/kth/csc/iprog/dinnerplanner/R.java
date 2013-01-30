f1af6a1ce693b196dfefe08d94e9dbaada618c11
Jens Ståhl <cykeltillsalu@n176-p111.kthopen.kth.se>
2013-01-30 14:49:37 +0100

    fu

--- gen/se/kth/csc/iprog/dinnerplanner/R.java
+++ gen/se/kth/csc/iprog/dinnerplanner/R.java
@@ -21,28 +21,33 @@ public final class R {
     }
     public static final class id {
         public static final int back_appetizer=0x7f070007;
-        public static final int back_ingredient=0x7f07000b;
+        public static final int back_desc=0x7f07000d;
+        public static final int back_ingredient=0x7f07000f;
         public static final int back_to_ingredient=0x7f070009;
         public static final int changeNumberOfGuests=0x7f070002;
         public static final int choose_header=0x7f070005;
         public static final int create_menu=0x7f070001;
-        public static final int dialogButtonOK=0x7f07000e;
-        public static final int menu_settings=0x7f07000f;
+        public static final int dialogButtonOK=0x7f070012;
+        public static final int dishImage=0x7f07000b;
+        public static final int ingredienser=0x7f07000a;
+        public static final int menu_settings=0x7f070013;
         public static final int next_appetizer=0x7f070008;
-        public static final int num_guests_input=0x7f07000d;
+        public static final int num_guests_input=0x7f070011;
+        public static final int recept=0x7f07000c;
         public static final int shopplistImage=0x7f070003;
         public static final int starters_table=0x7f070006;
         public static final int startsidan=0x7f070000;
         public static final int sumPrice=0x7f070004;
-        public static final int tableLayout1=0x7f07000a;
-        public static final int text=0x7f07000c;
+        public static final int tableLayout1=0x7f07000e;
+        public static final int text=0x7f070010;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int appetizer=0x7f030001;
         public static final int desc=0x7f030002;
-        public static final int ingredient=0x7f030003;
-        public static final int number_guests=0x7f030004;
+        public static final int description=0x7f030003;
+        public static final int ingredient=0x7f030004;
+        public static final int number_guests=0x7f030005;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
