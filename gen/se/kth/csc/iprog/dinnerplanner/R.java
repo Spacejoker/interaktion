17a3935eb96d6da23272606c3bfd7d6f136615c9
Jens Ståhl <cykeltillsalu@n176-p111.kthopen.kth.se>
2013-01-30 12:06:58 +0100

    hej

--- gen/se/kth/csc/iprog/dinnerplanner/R.java
+++ gen/se/kth/csc/iprog/dinnerplanner/R.java
@@ -11,26 +11,62 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int bakedbrie=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int icecream=0x7f020002;
-        public static final int meatballs=0x7f020003;
-        public static final int sourdough=0x7f020004;
+        public static final int cell_shape=0x7f020000;
+        public static final int diningtable=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int icecream=0x7f020003;
+        public static final int meatballs=0x7f020004;
         public static final int toast=0x7f020005;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int TextView01=0x7f070013;
+        public static final int TextView02=0x7f070012;
+        public static final int TextView03=0x7f070011;
+        public static final int TextView04=0x7f070010;
+        public static final int back_appetizer=0x7f070009;
+        public static final int back_ingredient=0x7f07001c;
+        public static final int back_to_ingredient=0x7f07000b;
+        public static final int changeNumberOfGuests=0x7f070002;
+        public static final int create_menu=0x7f070001;
+        public static final int dialogButtonOK=0x7f07001e;
+        public static final int menu_settings=0x7f07001f;
+        public static final int next_appetizer=0x7f07000a;
+        public static final int radio_ambi1=0x7f070008;
+        public static final int radio_group1=0x7f070005;
+        public static final int radio_left1=0x7f070007;
+        public static final int radio_right1=0x7f070006;
+        public static final int shopplistImage=0x7f070003;
+        public static final int startsidan=0x7f070000;
+        public static final int sumPrice=0x7f070004;
+        public static final int tableLayout1=0x7f07000c;
+        public static final int tableRow1=0x7f07000f;
+        public static final int tableRow2=0x7f070015;
+        public static final int tableRow4=0x7f07000d;
+        public static final int text=0x7f07001d;
+        public static final int textView1=0x7f070014;
+        public static final int textView2=0x7f070016;
+        public static final int textView3=0x7f070017;
+        public static final int textView4=0x7f070018;
+        public static final int textView5=0x7f070019;
+        public static final int textView6=0x7f07001a;
+        public static final int textView7=0x7f07001b;
+        public static final int textView9=0x7f07000e;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int appetizer=0x7f030001;
+        public static final int desc=0x7f030002;
+        public static final int ingredient=0x7f030003;
+        public static final int number_guests=0x7f030004;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
+        public static final int create=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int ultimate=0x7f040003;
     }
     public static final class style {
         /** 
--- src/se/kth/csc/iprog/dinnerplanner/MainActivity.java
+++ src/se/kth/csc/iprog/dinnerplanner/MainActivity.java
@@ -3,21 +3,99 @@ package se.kth.csc.iprog.dinnerplanner;
 import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+@SuppressLint("ShowToast")
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setupStart();
+		
+	}
+
+	private void setupStart() {
 		setContentView(R.layout.activity_main);
 		
 		//To get the dinner model you can use the following code:
 		DinnerModel model = ((DinnerPlannerApplication) this.getApplication()).getModel();
 		
-	}
+		Button btnDownload = (Button) findViewById(R.id.create_menu);
+		btnDownload.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Context context = v.getContext();
+				final Dialog dialog = new Dialog(context);
+				dialog.setContentView(R.layout.number_guests);
+				dialog.setTitle("Nr of guests");
+	
+				Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
+				// if button is clicked, close the custom dialog
+				dialogButton.setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						dialog.dismiss();
+						showAppetizer();
+						
+						
+						
+						//back from ingredients
+//						Button backIngridient = (Button) findViewById(R.id.back_ingredient);
+//						backIngridient.setOnClickListener(new View.OnClickListener() {
+//							
+//							@Override
+//							public void onClick(View v) {
+//								setupStart();
+//								setContentView(R.layout.activity_main);
+//							}
+//
+//						});
+					}
 
+				});
+	 
+				dialog.show();
+			}
+		});
+		
+	}
+	private void showAppetizer() {
+		setContentView(R.layout.appetizer);
+		Button next = (Button) findViewById(R.id.next_appetizer);
+		
+		next.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast toast = Toast.makeText(getBaseContext(), "abc", 1900);
+				toast.show();
+			}
+		});
+		
+		Button back = (Button) findViewById(R.id.back_appetizer);
+		
+		back.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast toast = Toast.makeText(getBaseContext(), "back", 1900);
+				toast.show();
+			}
+		});
+		
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
